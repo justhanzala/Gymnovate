3ad4ccd335769254e13030d3338b26ed594baf95
justhanzala <justhanzala786@gmail.com>
2020-03-28 12:33:17 +0530

    add empty not insert, bug fixed

--- create_user.php
+++ create_user.php
@@ -2,43 +2,46 @@
 include "functions.php";
 include "header.php";
 if(isset($_POST["createUserBtn"])){
+    $userName = $_POST["create-username"];
 	$createPassword = $_POST["create-password"];
-	$confirmPassword = $_POST["confirm-password"];
-    if($createPassword == $confirmPassword){
-        $createUser = insertuser();
-        ?>
-        <script type="text/javascript">
-            Swal.fire({
-                position: 'top',
-                icon: 'success',
-                title: 'Your account has been successfully created!',
-                showConfirmButton: false,
-                timer: 1500
-            });
-		</script>
+    $confirmPassword = $_POST["confirm-password"];
+    if(!empty($userName) && !empty($createPassword) && !empty($confirmPassword)){
+        if($createPassword == $confirmPassword){
+            $createUser = insertuser();
+            ?>
+            <script type="text/javascript">
+                Swal.fire({
+                    position: 'top',
+                    icon: 'success',
+                    title: 'Your account has been successfully created!',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            </script>
 
-        <?php
-    }else{
-        ?>
-        <script type="text/javascript">
-            Swal.fire({
-                position: 'top',
-                icon: 'error',
-                title: 'password was wrong!',
-                showConfirmButton: false,
-                timer: 1500
-            });
-		</script>
+            <?php
+        }else{
+            ?>
+            <script type="text/javascript">
+                Swal.fire({
+                    position: 'top',
+                    icon: 'error',
+                    title: 'password was wrong!',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            </script>
 
-        <?php
+            <?php
+        }
     }
 }
 ?>
 
 <div class="bg-light mt-4 p-4">
 <article class="mx-auto" style="max-width: 400px;">
-	<h4 class="card-title mt-3 text-center">Create Account</h4>
-	<p class="text-center">Get started with your free account</p>
+	<h4 class="card-title mt-3 text-center">Create new user</h4>
+	<p class="text-center">Get started with your new account</p>
 	<p class="divider-text">
         <span class="bg-light">OR</span>
     </p>
@@ -47,19 +50,19 @@ if(isset($_POST["createUserBtn"])){
             <div class="input-group-prepend">
                 <span class="input-group-text"> <i class="fa fa-user"></i> </span>
             </div>
-            <input name="create-username" class="form-control" placeholder="Username" type="text">
+            <input name="create-username" class="form-control" placeholder="Username" type="text" required autofocus>
         </div>
         <div class="form-group input-group">
             <div class="input-group-prepend">
                 <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
             </div>
-            <input name="create-password" class="form-control" placeholder="Create password" type="password">
+            <input name="create-password" class="form-control" placeholder="Create password" type="password" required>
         </div>
         <div class="form-group input-group">
             <div class="input-group-prepend">
                 <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
             </div>
-            <input name="confirm-password" class="form-control" placeholder="Confirm password" type="password">
+            <input name="confirm-password" class="form-control" placeholder="Confirm password" type="Text" required>
         </div>
         <div class="form-group">
             <button type="submit" name="createUserBtn" class="btn btn-primary btn-block"> Create Account  </button>
